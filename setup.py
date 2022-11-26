6785ea8cf05d85afb3fb342b136046fa6dd830aa
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 15:51:20 +0100

    edit version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1" 
+VERSION = "1.1" 
 DESCRIPTION = "Helpful modules for pytorch"
 LONG_DESCRIPTION = "Mini package, machine learning help modules for pytorch"
 
