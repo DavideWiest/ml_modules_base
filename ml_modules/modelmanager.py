962f3db995eea7254f9e54f58ffb8a680d1c5e7b
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 14:16:53 +0100

    init with test fn

---
+++ ml_modules/modelmanager.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+from torch import nn
+
+class ModelManager():
+    """
+    
+    """
+    def __init__(self):
+        pass
+
+    def save(self, name="{modelname}_{loss}", dir="models", subdir=None):
+        pass
+
+    def load(self, name="{modelname}_{loss}", dir="models", subdir=None):
+        pass
+
+    def test(self):
+        print("test successful")
+
---
+++ ml_modules/paramoptim.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+from torch import nn
+
+class ParameterOptimizer():
+    """
+    
+    """
+    def __init__(self, model: nn.Module, variance: int, perf_importance: int):
+        pass
