364d6c242bad8fd31dc662cec96e0ac5e5aeee06
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 16:57:02 +0100

    edit init

--- ml_modules/__init__.py
+++ ml_modules/__init__.py
@@ -1,4 +1,4 @@
 from ml_modules.modelmanager import ModelManager
 from ml_modules.hyparamoptim import HyParamOptim
-from ml_modules.runner import train_model
+from ml_modules.runner import *
 from ml_modules.funcs import *
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.3" 
+VERSION = "1.4" 
 DESCRIPTION = "Helpful modules for pytorch"
 LONG_DESCRIPTION = "Mini package, machine learning help modules for pytorch"
 
