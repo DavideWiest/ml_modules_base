e7ee59f34ae8d83bc4465c07cb3c380bab2abe7a
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 14:32:12 +0100

    edit setup py

--- setup.py
+++ setup.py
@@ -15,11 +15,10 @@ setup(
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=[
-            "pytorch",
+            "torch",
             "numpy",
             "pandas",
             "matplotlib",
-            "pytorch",
             "torchvision",
             "tqdm",
         ],
