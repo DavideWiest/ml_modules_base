33425ae71d6b92e906af89261a4705b3bc308e15
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 16:27:43 +0100

    small change

--- ml_modules/runner.py
+++ ml_modules/runner.py
@@ -58,7 +58,7 @@ def test_step(model: nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn
 
 
 
-def train_full_fn(model: nn.Module, train_dataloader: DataLoader, test_dataloader: DataLoader, optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, accuracy_fn, epochs: int, device, save_each=2, save_results_location="results.json", compare_saved_metric="loss", early_stop_epoch=None):
+def train_full_fn(model: nn.Module, train_dataloader: DataLoader, test_dataloader: DataLoader, optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, accuracy_fn, epochs: int, device, save_each=2, save_results_location="results.json", compare_saved_metric="loss", early_stop_epoch=None, logging=None):
     """
     wrapper function to train and test model
     """
@@ -79,7 +79,7 @@ def train_full_fn(model: nn.Module, train_dataloader: DataLoader, test_dataloade
         results["test_loss"].append(float(test_loss))
         results["test_acc"].append(float(test_acc))
 
-        print(f"\n\nEpoch {epoch} | train loss: {train_loss:.3f} | train acc: {(train_acc*100):.2f}% | test loss: {test_loss:.3f} | test acc: {(test_acc*100):.2f}% \n")
+        logging.info(f"\n\nEPOCH {epoch} | Train loss: {train_loss:.3f} | Train acc: {(train_acc*100):.2f}% | Test loss: {test_loss:.3f} | Test acc: {(test_acc*100):.2f}% \n")
 
         with open(save_results_location, "w", encoding="utf-8") as f:
             json.dump(results, f, indent=4)
@@ -91,7 +91,9 @@ def train_full_fn(model: nn.Module, train_dataloader: DataLoader, test_dataloade
         if early_stop_epoch != None:
             recent_test_loss = min(results["test_loss"][-5:])
             recent_test_acc = max(results["test_acc"][-5:])
-            
+
+            if recent_test_loss > min(results["test_loss"][:-5]) and recent_test_acc > min(results["test_acc"][:-5]):
+                logging.info(f"Stopping early with EPOCH {epoch}")
 
     
     return results
